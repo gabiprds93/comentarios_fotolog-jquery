aae16cf65905094a3888cf610390cf6eb41ba57f
gabiprds93 <gabiprds.93@gmail.com>
2017-09-08 16:41:43 -0500

    Ejercicio con jquery

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const app =
+{
+    item: 
+    {
+        name: undefined,
+        comment: undefined
+    },
+    
+    init: function() 
+    {
+        app.item.name = $('#clave');
+        app.item.comment = $('#valor');
+        app.showComments();
+        app.setup();
+    },
+
+    setup: function()
+    {
+        $('#btnGuardar').click(app.addComment);
+        $('#btnLimpiar').click(app.clearComments);
+    },
+
+    addComment: function() 
+    {
+        localStorage.setItem(app.item.name.val(), app.item.comment.val());
+        app.showComments();
+    },
+    
+    showComments: function()
+    {
+        $('#comentarios').empty();
+        for(var clave in localStorage)
+        {
+            $('#comentarios').append(`<p>${clave}</p>\
+            <p>${localStorage[clave]}</p>`);
+        }
+    },
+    
+    clearComments: function()
+    {
+        $('#comentarios').empty();
+        localStorage.clear();
+    }
+};
+$(document).ready(app.init);
